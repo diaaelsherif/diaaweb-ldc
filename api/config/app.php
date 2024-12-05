687c7b5b8f97865dafbc23cb433956236c59f065
Diaa Elsherif <diaaeldin.elsherif.ext@kiongroup.com>
2024-12-05 14:31:52 +0200

    start page and composer

--- api/config/app.php
+++ api/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://diaaweb.com/api'),
 
     /*
     |--------------------------------------------------------------------------
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:eHNhaWhldHFlcDZnZHdsdXh6bjRsdmJ2eW1xdHN5d2M='),
 
     'previous_keys' => [
         ...array_filter(
