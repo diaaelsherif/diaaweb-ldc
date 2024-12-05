de02501234bca94de0ac8d26cf813c969216d398
Diaa Elsherif <diaaeldin.elsherif.ext@kiongroup.com>
2024-12-05 03:22:28 +0200

    request validation

--- api/app/Http/Requests/StoreCourseRequest.php
+++ api/app/Http/Requests/StoreCourseRequest.php
@@ -3,7 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 class StoreCourseRequest extends FormRequest
 {
     /**
@@ -11,7 +12,7 @@ class StoreCourseRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,17 @@ class StoreCourseRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'description' => 'required'
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
     }
 }
