e990df4ad95b5e1b46ac0fa2c145fa08ca114190
Diaa Elsherif <diaaeldin.elsherif.ext@kiongroup.com>
2024-12-05 03:12:11 +0200

    create course respository class

---
+++ api/app/Repositories/CourseRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repository;
+use App\Models\Course;
+use App\Interfaces\CourseRepositoryInterface;
+class CourseReposiotry implements CourseRepositoryInterface
+{
+    public function index(){
+        return Course::all();
+    }
+
+    public function getById($id){
+       return Course::findOrFail($id);
+    }
+
+    public function store(array $data){
+       return Course::create($data);
+    }
+
+    public function update(array $data,$id){
+       return Course::whereId($id)->update($data);
+    }
+
+    public function delete($id){
+       Course::destroy($id);
+    }
+}
