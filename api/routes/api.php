ce5cfbda75c50d67eea8c4c51be504cf3ad0585f
Diaa Elsherif <diaa.elsherif@msn.com>
2025-05-17 05:52:01 +0300

    Adjusted routes of the api.

--- api/routes/api.php
+++ api/routes/api.php
@@ -8,4 +8,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::apiResource('/courses',CourseController::class);
+Route::get('/courses', [CourseController::class, 'index']);
+Route::get('/courses/{id}', [CourseController::class, 'show']);
+Route::post('/courses', [CourseController::class, 'store']);
+Route::put('/courses/{id}', [CourseController::class, 'update']);
+Route::delete('/courses/{id}', [CourseController::class, 'destroy']);
