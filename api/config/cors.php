ab71787295a91ce1349d789c5b5101b6081198cc
Diaa Elsherif <diaa.elsherif@msn.com>
2025-05-18 04:05:03 +0300

    Added cors.php

---
+++ api/config/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paths' => ['*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://my.diaaweb.com'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
