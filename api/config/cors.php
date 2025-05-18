e5bb4c0d47dd523dca250ff0a911acec6c3b7654
Diaa Elsherif <diaa.elsherif@msn.com>
2025-05-18 11:04:53 +0300

    Modified cors.

--- api/config/cors.php
+++ api/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://my.diaaweb.com'],
+    'allowed_origins' => ['https://my.diaaweb.com', 'https://diaaweb.netlify.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
