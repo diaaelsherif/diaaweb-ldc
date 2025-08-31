e6efe338109e5342e7cae46e0b2ea0816c6d24eb
Diaa Elsherif <diaaeldin.elsherif.ext@kiongroup.com>
2025-08-31 21:32:33 +0300

    testing

--- api/routes/api.php
+++ api/routes/api.php
@@ -10,7 +10,8 @@ Route::get('/user', function (Request $request) {
 
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/login', [AuthController::class, 'login'])->name('login');
 
 // Protected routes
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
