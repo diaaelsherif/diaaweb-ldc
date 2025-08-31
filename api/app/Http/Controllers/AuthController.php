136d56c659d1c9d8fe56b73d9ffb883b562d4001
Diaa Elsherif <diaaeldin.elsherif.ext@kiongroup.com>
2025-08-31 08:59:39 +0300

    test api

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -9,74 +9,66 @@ use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-  public function register(Request $request)
-  {
-      $validated = $request->validate([
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
         'name' => 'required|string|max:255',
         'email' => 'required|email|unique:users',
         'password' => 'required|string|min:8|confirmed',
-      ]);
+        ]);
 
-      $user = User::create($validated);
+        $user = User::create($validated);
 
-      if ($user)
-      {
-        $token = $user->createToken($user->name.'Auth-Token')->plainTextToken;
+        if ($user)
+        {
+            $token = $user->createToken($user->name.'Auth-Token')->plainTextToken;
 
-        Auth::login($user);
+            // Return data
+            return response()->json([
+                'message' => 'User registered successfully',
+                'token_type' => 'Bearer',
+                'token' => $token
+            ], 201);
+        }
+        else
+        {
+            return response()->json([
+                'message' => 'User not registered successfully',
+            ], 500);
+        }
+    }
 
-        // Return data
-        return response()->json([
-            'message' => 'User registered successfully',
-            'token_type' => 'Bearer',
-            'token' => $token
-        ], 201);
-      }
-      else
-      {
-        return response()->json([
-            'message' => 'User not registered successfully',
-        ], 500);
-      }
-  }
-
-  public function login(Request $request)
-  {
-      $validated = $request->validate([
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
         'email' => 'required|email',
         'password' => 'required|string|min:8',
-      ]);
+        ]);
 
-      if (Auth::attempt($validated)) {
-        $request->session()->regenerate();
+        $user = User::where('email',$request->email)->first();
 
-        $user = $request->user();
+        if(!$user || !Hash::check($request->password, $user->password)){
+        return response()->json([
+            'message' => 'Incorrect credentials'
+        ], 401);
+        }
 
         $token = $user->createToken($user->name.'Auth-Token')->plainTextToken;
 
         // Return data
         return response()->json([
-            'message' => 'User successfully logged in',
-            'token_type' => 'Bearer',
-            'token' => $token
+        'message' => 'User successfully logged in',
+        'token_type' => 'Bearer',
+        'token' => $token
         ], 200);
-      }
-
-      throw ValidationException::withMessages([
-        'credentials' => 'Sorry, incorrect credentials',
-      ]);
-  }
+    }
 
     public function logout(Request $request)
     {
-        $user = Auth::user();
+        $user = User::where('id', $request->user()->id)->first();
         if ($user)
         {
             $user->tokens()->delete();
-            Auth::logout();
-
-            $request->session()->invalidate();
-            $request->session()->regenerateToken();
 
             return response()->json([
                 'message' => 'User logged out successfully',
