f1d82dbcf9554791ddfadf098631edcb3d457ec2
Diaa Elsherif <diaaeldin.elsherif.ext@kiongroup.com>
2025-08-31 05:51:42 +0300

    auth

--- api/routes/api.php
+++ api/routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('guest')->controller(AuthController::class)->group(function ()
 });
 
 // courses
-Route::middleware('auth:sanctum')->controller(CourseController::class)->group(function () {
+Route::middleware('auth')->controller(CourseController::class)->group(function () {
     Route::get('/courses', [CourseController::class, 'index']);
     Route::get('/courses/{id}', [CourseController::class, 'show']);
     Route::post('/courses', [CourseController::class, 'store']);
